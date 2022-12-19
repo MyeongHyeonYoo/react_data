f26207d113b986032a80d0f9e815e749f6141b90
MyeongHyeonYoo <victorea5@naver.com>
2022-12-19 12:23:35 +0900

    react_data

---
+++ src/App19_1.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import React from 'react';
+
+function App() {
+      //return React.createElement("div", null, "hello", React.createElement("b", null, "react"));
+
+
+  return (
+    <div>
+      Hello <b>react</b>
+    </div>
+  );
+}
+
+
+export default App;
